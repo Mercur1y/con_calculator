a4c8abadd14cff2bd84e4b0661e916e6920ba92f
Mercury <pristavka.gleb@yandex.ru>
2022-05-06 19:26:14 +0400

    Создан проект

---
+++ src/main/java/com/simbirsoft/con_calc/ConCalcApplication.java
@@ -0,0 +1,13 @@
+package com.simbirsoft.con_calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConCalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConCalcApplication.class, args);
+    }
+
+}
